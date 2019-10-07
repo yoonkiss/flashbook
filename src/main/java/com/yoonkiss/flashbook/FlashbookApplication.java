ce9d6e9f3ba700124553398e5b70e5a4ef313c67
park.yoonki <park.yoonki@gmail.com>
2019-10-07 17:12:40 +0900

    prj initialized

---
+++ src/main/java/com/yoonkiss/flashbook/FlashbookApplication.java
@@ -0,0 +1,13 @@
+package com.yoonkiss.flashbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashbookApplication.class, args);
+	}
+
+}
